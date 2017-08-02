49a52d72c0808ea7f1106c36b7d790e5a48e35ad
mthouseef <muhammadthouseef05@gmail.com>
2017-08-02 13:31:05 +0530

    create ASGMT1.php
    assignment1

---
+++ sprint1/ASGMT1.php
@@ -0,0 +1,14 @@
+<?php
+
+$a=array("Arun","Ajay","Amritha","Athul","Akila");
+$alength=count($a);
+//foreach ($a as $key => $value) {
+	# code...
+
+for($i=0;$i<$alength;$i++)
+	{
+		echo $a[$i];
+		echo "<br>";
+	}
+
+?>
